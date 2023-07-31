a7e93387525d69ee0c2348765b95032d03b7b9a3
Kevin <k1den@rambler.ru>
2023-07-31 21:33:54 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,12 @@
 package ru.netology;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class PhoneBook {
+    private static Map<String,String> phoneBook = new TreeMap<>();
+    public int add(String name, String numberPhone){
+        phoneBook.put(name, numberPhone);
+        return phoneBook.size();
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+import org.junit.Assert;
+import ru.netology.PhoneBook;
+
 public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    public void addTest(){
+        final int addPhonebookExpected = 1;
+        final int actual = phoneBook.add("Денис" , "395521");
+        Assert.assertEquals(addPhonebookExpected, actual);
+    }
 }
