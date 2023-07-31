69d6b48dc5e170fadccf0256d7c62b3dec7fe6fb
Kevin <k1den@rambler.ru>
2023-07-31 21:44:45 +0300

    Stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -4,11 +4,16 @@ import ru.netology.PhoneBook;
 
 public class PhoneBookTest {
     PhoneBook phoneBook = new PhoneBook();
-
     @Test
     public void addTest(){
         final int addPhonebookExpected = 1;
         final int actual = phoneBook.add("Денис" , "395521");
         Assert.assertEquals(addPhonebookExpected, actual);
+    }
+    @Test
+    public void findByNumber(){
+        final String findPhonebookExpected = "Денис";
+        final String actual = phoneBook.findByNumber("395521");
+        Assert.assertEquals(findPhonebookExpected, actual);
     }
 }
